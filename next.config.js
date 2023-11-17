bf858f676fcf26d306d3512481c1a3a256103791
lensprotocolll <alivsadr@gmail.com>
2023-11-17 21:05:38 +0330

    fix

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  images: {
+    loader: "akamai",
+    path: "/",
+    // domains: ["user-images.githubusercontent.com"],
+  },
+  basePath: "",
+  assetPrefix: "",
+};
+
+module.exports = {
+  images: {
+    loader: "akamai",
+    path: "/",
+    // domains: ["user-images.githubusercontent.com"],
+  },
+  basePath: "",
+  assetPrefix: "",
+};
